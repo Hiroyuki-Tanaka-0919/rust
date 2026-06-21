b1f7b7618125967be4bf69d27408008c81a84025
Hiroyuki Tanaka <ht0919@gmail.com>
2026-06-21 18:12:00 +0900

    Chg README.md

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,7 +1,9 @@
-fn main(){
-    letreference_to_nothing=dangle();
-}
-fn dangle()->&String {
+fn dangle() -> String {
     let s = String::from("hello");
-    &s
+    s
+}
+
+fn main() {
+    let reference_to_nothing = dangle();
+    println!("{}", reference_to_nothing);
 }
